1f6147fdf303d5ef95a81a70ad1cea2e6465bdbb
Kaare Nilsen <kaare@workingbee.local>
2011-03-01 12:15:44 +0100

    o after adding <mvc:annotation-driven/> all my custom beans did not work anymore. But hey, I just autowired in the ones spring are registering, and then augment them.. nice :)

--- auction/src/main/java/no/arktekk/training/spring/config/MvcConfig.java
+++ auction/src/main/java/no/arktekk/training/spring/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package no.arktekk.training.spring.config;
 
 import no.arktekk.training.spring.util.AnnotationModelAndViewResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,17 +10,21 @@ import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.handler.AbstractUrlHandlerMapping;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
 import org.springframework.web.servlet.mvc.annotation.DefaultAnnotationHandlerMapping;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.lang.annotation.Annotation;
 
 /**
  * Config file that sets up Spring MVC the way I would like for the auction application.
  * <p/>
  * Uses jspx as view, and also incorporates a little tweeks so that we can use "/" as
  * servlet mapping, and our very own way of determining view names.
- *
+ * <p/>
  * Intentionally not a @Configuration class so that it is not picked up
  * by the main application context component scanning.
  * Needs to be added to the mvc context manually.
@@ -40,40 +45,12 @@ public class MvcConfig {
     }
 
     /**
-     * Since we are using "/" as servlet mapping, we need to force the spring controller
-     * servlet to use annotations as the first strategy for handling request mapping and
-     * model attribute handling. If we do not enforce order=0 for the mapping, then
-     * the webserver default servlet will try to handle the reuest, and tries to find
-     * static resources instead.
-     */
-    @Bean
-    public HandlerMapping handlerMapping() {
-        DefaultAnnotationHandlerMapping handlerMapping = new DefaultAnnotationHandlerMapping();
-        handlerMapping.setOrder(0);
-        return handlerMapping;
-    }
-
-    /**
-     * I really did not like the way Spring do not allow us to specify a concrete view name even when
-     * using annotations. The default rule is that the view name returned is the same as the one entered.
-     * I would prefer if I had an annotation on the method specifying both the view name, and optionally
-     * the model attribute name. So I created a custom view resolver to support this, and an annotation
-     * to specify it.
-     */
-    @Bean
-    public HandlerAdapter handlerAdapter() {
-        AnnotationMethodHandlerAdapter annotationMethodHandlerAdapter = new AnnotationMethodHandlerAdapter();
-        annotationMethodHandlerAdapter.setCustomModelAndViewResolver(new AnnotationModelAndViewResolver());
-        return annotationMethodHandlerAdapter;
-    }
-
-    /**
      * Registeres the file labels.properties, labels_nb_NO.properties ...
      * as a source for labels to be used in the jspx files.
      * Uses standards java resource bundle stragegies
      */
     @Bean
-    public MessageSource messageSource(){
+    public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("labels");
         return messageSource;
@@ -84,7 +61,37 @@ public class MvcConfig {
      * tries to find the accept-language header from the browser.
      */
     @Bean
-    public LocaleResolver localeResolver(){
-         return new AcceptHeaderLocaleResolver();
+    public LocaleResolver localeResolver() {
+        return new AcceptHeaderLocaleResolver();
+    }
+
+    /**
+     * Since we are using "/" as servlet mapping, we need to force the spring controller
+     * servlet to use annotations as the first strategy for handling request mapping and
+     * model attribute handling. If we do not enforce order=0 for the mapping, then
+     * the webserver default servlet will try to handle the reuest, and tries to find
+     * static resources instead.
+     */
+    @Autowired
+    public void handlerMapping(DefaultAnnotationHandlerMapping handlerMapping) {
+        handlerMapping.setOrder(0);
+    }
+
+    /**
+     * I really did not like the way Spring do not allow us to specify a concrete view name even when
+     * using annotations. The default rule is that the view name returned is the same as the one entered.
+     * I would prefer if I had an annotation on the method specifying both the view name, and optionally
+     * the model attribute name. So I created a custom view resolver to support this, and an annotation
+     * to specify it.
+     */
+    @Autowired
+    public void handlerAdapter(AnnotationMethodHandlerAdapter adapter) {
+        adapter.setCustomModelAndViewResolver(new AnnotationModelAndViewResolver());
+    }
+
+    public void registerStaticPaths(ResourceHttpRequestHandler handler){
+
+        handler.
+
     }
 }
